a9e69f3c5e3ef21e7823db0192893069bac098b8
MohamedDarkaoui <mohameddarkaoui.md@gmail.com>
2023-05-24 02:09:15 +0200

    test for the Server's getPlayer method

---
+++ megamek/unittests/megamek/server/ServerTest.java
@@ -0,0 +1,26 @@
+package megamek.server;
+import megamek.common.Game;
+import megamek.common.IGame;
+import megamek.common.IPlayer;
+import megamek.common.Player;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+@RunWith(JUnit4.class)
+public class ServerTest {
+
+    @Test
+    public void getPlayerTest() throws IOException {
+        Server server = new Server("password", 483);
+        IGame game = new Game();
+        IPlayer player1 = new Player(1, "tester1");
+        game.addPlayer(player1.getId(), player1);
+        server.setGame(game);
+        Assert.assertEquals(server.getPlayer(1), player1);
+    }
+}
